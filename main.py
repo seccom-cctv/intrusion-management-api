3f3495f5d0bd38372ae7e815c632fdad59d1983d
Dinis-Lei <dinislei@ua.pt>
2022-11-26 21:33:09 +0000

    upload file to S3 endpoint

--- main.py
+++ main.py
@@ -1,55 +1,38 @@
 from typing import Union
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile
 from consumer import Video_Consumer
-import asyncio
 import threading
+import boto3
+import shutil
+import os
 
 app = FastAPI()
 
 items = {"foo": "The Foo Wrestlers"}
 
 consumer_thread = None
-
-@app.on_event('startup')
-async def startup_event():
-    print("Startup...")
-    # AMQP Variables
-    RABBIT_MQ_URL = "localhost:5672"
-    RABBIT_MQ_USERNAME = "myuser"
-    RABBIT_MQ_PASSWORD = "mypassword"
-    RABBIT_MQ_EXCHANGE_NAME = "human-detection-exchange"
-    RABBIT_MQ_QUEUE_NAME = "human-detection-queue"
-
-    # OUTPUT
-    OUTPUT_DIR = "intruders"
-
-    worker = Video_Consumer(OUTPUT_DIR)
-
-    kwargs = {
-            "broker_url": RABBIT_MQ_URL,
-            "broker_username": RABBIT_MQ_USERNAME,
-            "broker_password": RABBIT_MQ_PASSWORD,
-            "exchange_name": RABBIT_MQ_EXCHANGE_NAME,
-            "queue_name": RABBIT_MQ_QUEUE_NAME
-    }
-
-    consumer_thread = threading.Thread(
-        target=worker.start_processing, 
-        kwargs=kwargs
-    )
-    consumer_thread.start() 
-
-    print("End startup...")
-    
-@app.on_event('shutdown')
-async def shutdown():
-    print("Shutdown")
-    quit()
+#client = boto3.client('secretsmanager')
+s3 = boto3.client("s3")
 
 @app.get("/")
 async def read_root():
     '''Document endpoint usage here'''
 
     return {"Hello": "World"}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile =  File(...)):
+    fname = f"intruders/{file.filename}"
+    with open(fname, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    s3.upload_file(
+        Filename=fname,
+        Bucket="seccom-video-store",
+        Key=file.filename
+    )
+    os.remove(fname)
+
+    return {"filename": file.filename}
 
 

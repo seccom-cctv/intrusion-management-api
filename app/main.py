0f65d4c06c2079dc3696e2b7148a811fb25d35fe
Dinis-Lei <dinislei@ua.pt>
2022-12-07 15:23:29 +0000

    ECP-88 added listener to rabbit events and file struct refactor

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from typing import Union
 from fastapi import FastAPI, HTTPException, File, UploadFile
-from consumer import Video_Consumer
-import threading
+from fastapi.responses import RedirectResponse
 import boto3
 import shutil
 import os
+import threading
+from event_handler import myConsumer
 
 app = FastAPI()
 
@@ -14,14 +15,19 @@ consumer_thread = None
 #client = boto3.client('secretsmanager')
 s3 = boto3.client("s3")
 
+@app.on_event("startup") 
+async def startup_event():
+    consumer = myConsumer()
+    consumer.start_connection()
+    consumer_thread  =threading.Thread(target=consumer.run)
+    consumer_thread.start()
+
 @app.get("/")
-async def read_root():
-    '''Document endpoint usage here'''
+async def root():
+    return RedirectResponse(url='/docs')
 
-    return {"Hello": "World"}
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile =  File(...)):
+@app.post("/uploadfile/{id}")
+async def create_upload_file(id: int, file: UploadFile =  File(...)):
     fname = f"intruders/{file.filename}"
     with open(fname, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -36,3 +42,7 @@ async def create_upload_file(file: UploadFile =  File(...)):
     return {"filename": file.filename}
 
 
+def dispatch_alarm(alarm_id):
+    
+    
+    pass
